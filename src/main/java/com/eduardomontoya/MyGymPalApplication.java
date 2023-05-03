81ec5a5c974542463927d8bd854549f87c8a155a
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-05-03 11:17:04 -0500

    Initial Commit

---
+++ src/main/java/com/eduardomontoya/MyGymPalApplication.java
@@ -0,0 +1,13 @@
+package com.eduardomontoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGymPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGymPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardomontoya/MyGymPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardomontoya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGymPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
