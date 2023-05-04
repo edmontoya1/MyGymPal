f1d8f69d0d92e081fec5c357b0d30bdec82c3a23
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-05-04 13:20:29 -0500

    User Controller Initial Commit

---
+++ src/main/java/com/eduardomontoya/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.eduardomontoya.controller;
+
+import com.eduardomontoya.model.User;
+import com.eduardomontoya.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
