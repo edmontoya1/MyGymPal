5fe285be79310f3d6d5c5028779cee3feb985cad
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-05-04 13:21:17 -0500

    User Service Initial Commit

---
+++ src/main/java/com/eduardomontoya/service/UserService.java
@@ -0,0 +1,23 @@
+package com.eduardomontoya.service;
+
+import com.eduardomontoya.model.User;
+import com.eduardomontoya.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
